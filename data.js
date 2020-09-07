2b789c182cc658eff49f7d6ce664bca929cc3436
Jorge Luiz <44803139+ozzott@users.noreply.github.com>
2020-09-07 14:27:01 -0300

    Add data.js with some data

---
+++ data.js
@@ -0,0 +1,23 @@
+module.exports = [
+    {
+        id:"starter",
+        course_url:"https://storage.googleapis.com/golden-wind/bootcamp-launchbase/assets/starter.svg",
+        title:"Starter",
+        modules:"5 modules",
+        price:"Free"
+    },
+    {
+        id:"launchbase",
+        course_url:"https://storage.googleapis.com/golden-wind/bootcamp-launchbase/assets/launchbase.svg",
+        title:"Launchbase",
+        modules:"8 modules",
+        price:"Paid"
+    },
+    {
+        id:"gostack",
+        course_url:"https://storage.googleapis.com/golden-wind/bootcamp-launchbase/assets/gostack.svg",
+        title:"Gostack",
+        modules:"11 modules",
+        price:"Paid"
+    }
+]
