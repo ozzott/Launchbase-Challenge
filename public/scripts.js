3e076076bf1a8cd50aeb9a622c7d49672900150c
Jorge Luiz <44803139+ozzott@users.noreply.github.com>
2020-09-08 20:12:41 -0300

    Update scripts.js with URL redirect

--- public/scripts.js
+++ public/scripts.js
@@ -4,22 +4,6 @@ const cards = document.querySelectorAll('.card')
 for(let card of cards) {
     card.addEventListener("click", function() {
         const curseId = card.getAttribute('id');
-        modalOverlay.classList.add('active')
-        modalOverlay.querySelector("iframe").src = `https://www.rocketseat.com.br/${curseId}`;
+       window.location.href = `/courses/${curseId}`
     })
 }
-
-document.querySelector('.close-modal').addEventListener('click', function(){ 
-    modalOverlay.classList.remove('active')
-    modalOverlay.querySelector("iframe").src=""
-    modalOverlay.classList.remove("maximize");
-    
-})
-
-document.querySelector(".maximize-modal").addEventListener("click", function(){
-    modalOverlay.firstElementChild.classList.add("maximize");
-})
-
-document.querySelector(".minimize-modal").addEventListener("click", function(){
-    modalOverlay.firstElementChild.classList.remove("maximize");
-})
